e8751fe9c023d4fe817832698297a97829c18a89
Antonino <antoninoscaffidi@gmail.com>
2024-03-06 15:21:17 +0100

    Added navbar

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navbar"
+export default class extends Controller {
+  //static targets = [ "arrow", "menu", "closeButton"]
+  static targets = ["content"]
+
+    connect() {
+      this.close()
+    }
+  
+    toggle() {
+      if (this.contentTarget.classList.contains("hidden")) {
+        this.open()
+      }
+      else {
+        this.close()
+      }
+    }
+  
+    open() {
+      this.contentTarget.classList.remove("hidden")
+    }
+
+    close() {
+      this.contentTarget.classList.add("hidden")
+    }
+
+}
