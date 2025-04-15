2fdd4fb82453d87405d84d01544a4eb84f7f3e6e
Antonino <antoninoscaffidi@gmail.com>
2025-04-15 06:23:05 +0200

    Updated rails version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :null_session #Da inserire solo per utilizzare postman
+    #protect_from_forgery with: :null_session #Da inserire solo per utilizzare postman
+    allow_browser versions: :modern
 end
